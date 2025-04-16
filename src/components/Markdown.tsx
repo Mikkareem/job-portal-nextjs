02b0011c206e5fbd3a389052bf7c4ede8480505b
Mikkareem <mohdirsath1997@gmail.com>
2025-04-16 09:34:40 +0530

    Setup done

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,26 @@
+import ReactMarkdown from "react-markdown";
+
+interface MarkdownProps {
+  children: string;
+}
+
+export default function Markdown({ children }: MarkdownProps) {
+  return (
+    <div className="space-y-3">
+      <ReactMarkdown
+        components={{
+          ul: (props) => <ul className="list-inside list-disc" {...props} />,
+          a: (props) => (
+            <a
+              className="text-green-500 underline"
+              target="_blank"
+              {...props}
+            />
+          ),
+        }}
+      >
+        {children}
+      </ReactMarkdown>
+    </div>
+  );
+}
